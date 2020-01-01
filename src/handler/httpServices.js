62373d96f9c85f03c06aa9c7ceb93534c233592f
Unknown <joseph.juri@gmail.com>
2020-01-01 23:45:36 +0100

    Changed httpServices.js
    - Added message if no RestAPI URL is Provided

--- src/handler/httpServices.js
+++ src/handler/httpServices.js
@@ -5,10 +5,11 @@
 // ====================== //
 
 export function httpGet(url){
+    if(!url || url.length == 0){
+        return "No URL provided!";
+    }
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, false ); // false for synchronous request
     xmlHttp.send('Access-Control-Allow-Origin');
-    console.log(xmlHttp.responseText);
-    document.getElementById('response').innerHTML = xmlHttp.responseText;
     return xmlHttp.responseText;
 }
